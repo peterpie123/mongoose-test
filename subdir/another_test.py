aada909c2816f17c251c411f6dadc0e0ac44dadc
QuantumCreation <joseph6433@gmail.com>
2024-04-20 22:24:51 -0500

    more changes

--- subdir/another_test.py
+++ subdir/another_test.py
@@ -1,11 +1,9 @@
 from helper import subtract
 
 class MyClass:
-    def __init__(self):
-        pass
-    
+
     def subtract_wrapper(a, b):
-        return a - b
+        return subtract(a, b)
 
 def exponentiate(a, b):
     return a**b
