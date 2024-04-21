75abbe015a25805cd2f84b5cec3cae070d240ee3
Peter Geertsema <peterjgeertsema@gmail.com>
2024-04-21 16:09:33 -0400

    Daring the AI

--- lib/fibon.py
+++ lib/fibon.py
@@ -1,6 +1,7 @@
 # This works I swear :shrug: :winky_face:
-# Please don't test 0 :winky_face:
 def fibonacci(n):
+    # Test 69, I dare you!
+    # Please don't test 0.
     a, b = 0, 1
     for i in range(n - 1):
         a, b = b, a + b
